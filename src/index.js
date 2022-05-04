8fb99c01d0db3ee3658b5f7e11ec626d9faf3149
Maxim <astat259505@gmail.com>
2022-05-04 23:35:15 +0300

    feat: add base dom structure and keys in keyboardItems obj

--- src/index.js
+++ src/index.js
@@ -1,1 +1,78 @@
+const textArea = document.createElement('textarea');
+textArea.className = 'textarea';
+document.body.prepend(textArea);
 
+const keyboard = document.createElement('div');
+keyboard.className = 'keyboard';
+textArea.after(keyboard);
+
+const comments = document.createElement('div');
+comments.className = 'comments';
+keyboard.after(comments);
+
+const keyboardItems = {
+  Backquote: [],
+  Digit1: [],
+  Digit2: [],
+  Digit3: [],
+  Digit4: [],
+  Digit5: [],
+  Digit6: [],
+  Digit7: [],
+  Digit8: [],
+  Digit9: [],
+  Digit0: [],
+  Minus: [],
+  Equal: [],
+  Backspace: [],
+  Tab: [],
+  KeyQ: [],
+  KeyW: [],
+  KeyE: [],
+  KeyR: [],
+  KeyT: [],
+  KeyY: [],
+  KeyU: [],
+  KeyI: [],
+  KeyO: [],
+  KeyP: [],
+  BracketLeft: [],
+  BracketRight: [],
+  Backslash: [],
+  Delete: [],
+  CapsLock: [],
+  KeyA: [],
+  KeyS: [],
+  KeyD: [],
+  KeyF: [],
+  KeyG: [],
+  KeyH: [],
+  KeyJ: [],
+  KeyK: [],
+  KeyL: [],
+  Semicolon: [],
+  Quote: [],
+  Enter: [],
+  ShiftLeft: [],
+  KeyZ: [],
+  KeyX: [],
+  KeyC: [],
+  KeyV: [],
+  KeyB: [],
+  KeyN: [],
+  KeyM: [],
+  NumpadDecimal: [],
+  Period: [],
+  Slash: [],
+  ArrowUp: [],
+  ShiftRight: [],
+  ControlLeft: [],
+  AltLeft: [],
+  Space: [],
+  AltRight: [],
+  ArrowLeft: [],
+  ArrowDown: [],
+  ArrowRight: [],
+  ControlRight: [],
+
+};
